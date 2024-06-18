2cb17bd0350e68bde911ba69ba23341772f3b5db
pailodze <mikheilizee@gmail.com>
2024-06-18 20:27:24 +0400

    chore: added secret when veryfing

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -7,6 +7,7 @@ import {
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { jwtConfig } from '../../config/config';
 import { IS_PUBLIC_KEY } from './public.key';
 
 @Injectable()
@@ -34,8 +35,10 @@ export class AuthGuard implements CanActivate {
     }
 
     try {
-      const payload: { id: number } =
-        await this.jwtService.verifyAsync(accessToken);
+      const payload: { id: number } = await this.jwtService.verifyAsync(
+        accessToken,
+        { secret: jwtConfig.jwtSecret },
+      );
 
       request['user'] = payload;
     } catch (err) {
