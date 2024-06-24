cff4aaaa3bb19c4d430c973c2dee8383e2c49b4e
pailodze <mikheilizee@gmail.com>
2024-06-24 20:57:59 +0400

    chore: removed logging

--- src/db/orm.config.ts
+++ src/db/orm.config.ts
@@ -6,7 +6,6 @@ export const connectionOptions: DataSourceOptions = {
   ...databaseConfig,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*{.ts,.js}'],
-  logging: true,
 };
 
 const dataSource: DataSource = new DataSource(connectionOptions);
