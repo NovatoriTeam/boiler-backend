4b11545806f06103a6340f28f3e106b5ca9503b4
pailodze <mikheilizee@gmail.com>
2024-06-27 20:45:15 +0400

    refactor: wrapped use interceptoers with decorators

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -1,14 +1,8 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
+import { Public } from '../../auth/decorators/public.decorator';
 import { BaseController } from '../../base/controllers/base.controller';
-import { FilterInterceptor } from '../../base/interceptors/filter.interceptor';
+import { CrudFilter } from '../../base/decorators/crud-filter.decorator';
 import { RequestInterface } from '../../base/types/interfaces/request.interface';
 import { CreateProductDto } from '../dtos/create-product.dto';
 import { UpdateProductDto } from '../dtos/update-product.dto';
@@ -26,17 +20,17 @@ export class ProductsController extends BaseController<
     super(productService);
   }
 
-  @UseInterceptors(
-    new FilterInterceptor(
-      {
-        shop: ['sortable', 'searchable', 'exact'],
-        price: ['sortable', 'searchable'],
-        user: ['relatable'],
-      },
-      'product',
-    ),
+  @CrudFilter(
+    {
+      name: ['sortable'],
+      shop: ['sortable', 'searchable', 'exact'],
+      price: ['sortable', 'searchable'],
+      user: ['relatable'],
+    },
+    'product',
   )
   @ApiResponse({ isArray: true, type: Product })
+  @Public()
   @Get()
   async findAll(@Req() req: RequestInterface): Promise<[Product[], number]> {
     return await super.findAll(req);
